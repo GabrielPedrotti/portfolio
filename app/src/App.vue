c03aab94b052625a2dc324b2ab507f85a496b293
Gabriel Pedrotti da Silva <gabrielps2003@gmail.com>
2022-10-20 20:05:08 -0300

    chore: init

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const teste = 'abcd'
+
+const testMethod = (prop1, prop2) => {
+  console.log('testMethod')
+}
+//create a computed
+const testComputed = computed(() => {
+  return 'computed value'
+})
+
+</script>
---
+++ app/src/views/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h2>TESTERAADASD</h2>
+  </main>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const teste = "abcd";
+
+const testMethod = (prop1, prop2) => {
+  switch (prop1) {
+    case "teste1":
+      console.log("aaaa");
+      break;
+    case "teste2":
+      console.log("bbbb");
+      break;
+    default:
+      console.log("testMethod");
+      break;
+  }
+};
+//create a computed
+const testComputed = computed(() => {
+  return "computed value";
+});
+</script>
