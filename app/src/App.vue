009a8518f10ecff2ea49cb5f341d0c7d215b2358
Gabriel Pedrotti da Silva <gabrielps2003@gmail.com>
2022-10-20 22:13:41 -0300

    feat: initialized header and app front

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,19 @@
 <template>
+  <headerComponent />
   <RouterView />
 </template>
 
 <script setup>
 import { computed } from "vue";
+import headerComponent from "@/components/header/PageHeader.vue";
 
-const teste = 'abcd'
+const teste = "abcd";
 
 const testMethod = (prop1, prop2) => {
-  console.log('testMethod')
-}
+  console.log("testMethod");
+};
 //create a computed
 const testComputed = computed(() => {
-  return 'computed value'
-})
-
+  return "computed value";
+});
 </script>
